20f2195f2b4f8a1a5867cc7148f03ee56fe79f4f
Vaibhav Verma <vaibhavverma016@gmail.com>
2022-01-09 16:37:02 +0530

    setup added

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+## edit below variables as per your requirements -
+REPO_NAME = "dvc-nlp-usecase"
+AUTHOR_USER_NAME = "vibuverma"
+SRC_REPO = "src"
+LIST_OF_REQUIREMENTS = [
+    'dvc',
+    'pandas',
+    'scikit-learn',
+    'matplotlib',
+    'boto3',
+    'tqdm',
+    'PyYAML'
+]
+
+
+setup(
+    name=SRC_REPO,
+    version="0.0.1",
+    author=AUTHOR_USER_NAME,
+    description="A small package for DVC",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    author_email="vaibhavverma016@gmail.com",
+    packages=[SRC_REPO],
+    license="MIT",
+    python_requires=">=3.6",
+    install_requires=LIST_OF_REQUIREMENTS
+)
