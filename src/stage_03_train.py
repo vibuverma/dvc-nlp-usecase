632018de63d593a4444a9137ff8842add96bfbef
Vaibhav Verma <vaibhavverma016@gmail.com>
2022-01-10 19:06:49 +0530

    stage 3 completed

--- src/stage_03_train.py
+++ src/stage_03_train.py
@@ -5,9 +5,13 @@ from tqdm import tqdm
 import logging
 from src.utils.common import read_yaml, create_directories
 import random
+import joblib
+import numpy as np
+
+from sklearn.ensemble import RandomForestClassifier
 
 
-STAGE = "STAGE_NAME" ## <<< change stage name 
+STAGE = "Three" ## <<< change stage name 
 
 logging.basicConfig(
     filename=os.path.join("logs", 'running_logs.log'), 
@@ -21,7 +25,41 @@ def main(config_path, params_path):
     ## read config files
     config = read_yaml(config_path)
     params = read_yaml(params_path)
-    pass
+    artifacts= config["artifacts"]
+
+    featurized_data_dir_path= os.path.join(artifacts["ARTIFACTS_DIR"], artifacts["FEATURIZED_DATA"])
+    featurized_train_data_path= os.path.join(featurized_data_dir_path, artifacts["FEATURIZED_OUT_TRAIN"])
+
+    model_dir_path= os.path.join(artifacts["ARTIFACTS_DIR"], artifacts["MODEL_DIR"])
+    create_directories([model_dir_path])
+    model_path= os.path.join(model_dir_path, artifacts["MODEL_NAME"])
+
+    matrix= joblib.load(featurized_train_data_path)
+
+    labels = np.squeeze(matrix[:, 1].toarray())
+    X= matrix[:,2:]
+
+    logging.info(f"input matrix shape: {matrix.shape}\n")
+    logging.info(f"The shape of the X matrix is: {X.shape}\n")
+    logging.info(f"The shape of the labels matrix is: {labels.shape}\n")
+
+
+    seed= params["train"]["seed"]
+    n_estimators= params["train"]["n_estimators"]
+    min_split= params["train"]["min_split"]
+
+
+    model= RandomForestClassifier(
+        n_estimators=n_estimators,
+        min_samples_split=min_split,
+        random_state=seed,
+        n_jobs=-1
+    )
+
+    model.fit(X, labels)
+    joblib.dump(model, model_path)
+
+
 
 
 if __name__ == '__main__':
